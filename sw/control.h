f85601522c273b93635300159cc726bac8207dc5
Aaron Schulman <aschulm@stanford.edu>
2014-06-10 01:17:05 -0700

    removed extra get control commands that are not used yet

--- sw/control.h
+++ sw/control.h
@@ -4,11 +4,8 @@
 typedef enum CONTROL_TYPE_enum
 {
 	CONTROL_TYPE_AMPPOT_SET = 0,      // set the amp's potentiometer (gain)
-	CONTROL_TYPE_AMPPOT_GET,          // get the amp's potentiometer (gain)
 	CONTROL_TYPE_FILPOT_SET,          // set the LPF's potentiometer (cutoff freq)
-	CONTROL_TYPE_FILPOT_GET,          // get the LPF's potentiometer (cutoff freq)
 	CONTROL_TYPE_SAMPLE_TIMER_SET,    // set the sample timer (clock div and overflow) 
-	CONTROL_TYPE_SAMPLE_TIMER_GET,    // get the sample timer (clock div and overflow) 
 	CONTROL_TYPE_START_SAMPLING_UART, // start taking samples and sending them over the uart
 	CONTROL_TYPE_START_SAMPLING_SD,   // start taking samples and storing them on the SD card
 	CONTROL_TYPE_USB_POWER_SET,       // change the state of the USB power
