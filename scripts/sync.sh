eac5b75db595977401ce3a919bc545b826fd5177
Aaron Schulman <aschulman@google.com>
2015-02-24 09:42:27 -0800

    separated charging script from sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,13 +3,6 @@
 FLASH_FILE='/sys/class/leds/led:flash_1/brightness'
 FLASH_INT_S=0.01
 
-# disable charging
-echo 0xCA > /sys/kernel/debug/bq24192/INPUT_SRC_CONT
-chmod 644 /sys/class/power_supply/usb/online
-echo 0 > /sys/class/power_supply/usb/online
-
-sleep 1
-
 # drop sync signal with flash
 chmod 777 $FLASH_FILE
 i=0
@@ -25,7 +18,3 @@ do
 done
 echo 0 > $FLASH_FILE
 echo 0
-
-# enable charging
-echo 0x4A > /sys/kernel/debug/bq24192/INPUT_SRC_CONT
-echo 1 > /sys/class/power_supply/usb/online
