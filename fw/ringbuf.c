a099071704e486026bce9d81ddc8940e131b4998
Aaron Schulman <aschulm@stanford.edu>
2015-09-02 11:56:39 -0700

    (1) Fixed ring buffer so it takes into account the existing position in the data. (2) Added many new test cases for this issue and others.

--- fw/ringbuf.c
+++ fw/ringbuf.c
@@ -21,15 +21,18 @@ int ringbuf_write(ringbuf* rb, void* src, uint16_t len) //{{{
 	if ((rb->len + len) > rb->base_len)
 		return -1;
 	
-	while (len > 0)
+	uint16_t len_wrote = 0;
+
+	while (len_wrote < len)
 	{
 		uint16_t len_free = (rb->base_len - rb->write_idx);
-		uint16_t len_towrite = (len < len_free) ? len : len_free;
+		uint16_t len_towrite = ((len - len_wrote) < len_free) 
+			? (len - len_wrote) : len_free;
 
-		rb->write(rb->base + rb->write_idx, src, len_towrite);
+		rb->write(rb->base + rb->write_idx, src + len_wrote, len_towrite);
+		len_wrote += len_towrite;
 		rb->write_idx += len_towrite;
 		rb->len += len_towrite;
-		len -= len_towrite;
 
 		// reached end of buffer, wrap around
 		if (rb->write_idx >= rb->base_len)
@@ -45,15 +48,17 @@ int ringbuf_read(ringbuf* rb, void* dst, uint16_t len) //{{{
 	if (len > rb->len)
 		return -1;
 
-	while (len > 0)
+	uint16_t len_read = 0;
+	while (len_read < len)
 	{
 		uint16_t len_toend = (rb->base_len - rb->read_idx);
-		uint16_t len_toread = (len < len_toend) ? len : len_toend;
+		uint16_t len_toread = ((len - len_read) < len_toend) 
+			? (len - len_read) : len_toend;
 
-		rb->read(dst, rb->base + rb->read_idx, len_toread);
+		rb->read(dst + len_read, rb->base + rb->read_idx, len_toread);
+		len_read += len_toread;
 		rb->read_idx += len_toread;
 		rb->len -= len_toread;
-		len -= len_toread;
 
 		// reached end of buffer, wrap around
 		if (rb->read_idx >= rb->base_len)
@@ -69,7 +74,7 @@ int ringbuf_self_test() //{{{
 	uint8_t ring[100];
 
 	uint8_t test_src[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99};
-	uint8_t buf[10];
+	uint8_t buf[100];
 
 	printf("ringbuf self test\n");
 
@@ -157,6 +162,76 @@ int ringbuf_self_test() //{{{
 	if (ringbuf_read(&rb, buf, 2) >= 0)
 	{
 		printf("FAILED (ringbuf_read)\n");
+		return 1;
+	}
+	printf("PASSED\n");
+
+	printf("ringbuf past end write...");
+	memset(ring, 0, sizeof(ring));
+	memset(buf, 0, sizeof(buf));
+	ringbuf_init(&rb, ring, 4, &memcpy, &memcpy);
+	if (ringbuf_write(&rb, test_src, 1) < 0)
+	{
+		printf("FAILED (ringbuf_write #1)\n");
+		return 1;
+	}
+	if (ringbuf_read(&rb, buf, 1) < 0)
+	{
+		printf("FAILED (ringbuf_read #1)\n");
+		return 1;
+	}
+	if (ringbuf_write(&rb, test_src + 1, 4) < 0)
+	{
+		printf("FAILED (ringbuf_write #2)\n");
+		return 1;
+	}
+	if (ringbuf_read(&rb, buf, 4) < 0)
+	{
+		printf("FAILED (ringbuf_read #2)\n");
+		return 1;
+	}
+	if (memcmp(buf, test_src + 1, 4) != 0)
+	{
+		printf("FAILED (memcmp)\n");
+		return 1;
+	}
+	printf("PASSED\n");
+
+	printf("ringbuf SRAM multi write, read...");
+	uint8_t test_big_src[] =
+	{
+		0x00, 0x01, 0x02, 0x03,
+		0x10, 0x11, 0x12, 0x13,
+		0x20, 0x21, 0x22, 0x23,
+		0x30, 0x31, 0x32, 0x33,
+		0x40, 0x41, 0x42, 0x43,
+		0x50, 0x51, 0x52, 0x53
+	};
+	memset(buf, 0, sizeof(buf));
+	ringbuf_init(&rb, (void*)0x0000, 100, &sram_write, &sram_read);
+	if (ringbuf_write(&rb, test_big_src, 4) < 0)
+	{
+		printf("FAILED (ringbuf_write #1)\n");
+		return 1;
+	}
+	if (ringbuf_write(&rb, test_big_src + 4, 2) < 0)
+	{
+		printf("FAILED (ringbuf_write #2)\n");
+		return 1;
+	}
+	if (ringbuf_write(&rb, test_big_src + 6, 18) < 0)
+	{
+		printf("FAILED (ringbuf_write #3)\n");
+		return 1;
+	}
+	if (ringbuf_read(&rb, buf, 24) < 0)
+	{
+		printf("FAILED (ringbuf_read)\n");
+		return 1;
+	}
+	if (memcmp(buf, test_big_src, 24) != 0)
+	{
+		printf("FAILED (memcmp)\n");
 		return 1;
 	}
 	printf("PASSED\n");
