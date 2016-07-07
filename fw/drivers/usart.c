aschulm/battor

commit 42fbb5ace99e78775aefc019da4e4490b86c4172

Simplified USART BAUD rate setting.
There were many instances of bit munging to set the baud rate all over
the firmware. Now we have one function to do this. Doing this also
helped me catch another smaller issue which was that the SPI baud
rate was being miscalculated because we were not using the USART
BAUD rate formula for SPI Master mode.

fw/drivers/usart.c
@@ -30,3 +30,9 @@ void usart_spi_txrx(USART_t* usart, const void* txd, void* rxd, uint16_t len)
 			usart->DATA;
 	}
 }
+
+void usart_set_baud(USART_t* usart, uint16_t bsel, uint8_t bscale)
+{
+	usart->BAUDCTRLB = (bscale << USART_BSCALE_gp) | (bsel >> 8);
+	usart->BAUDCTRLA = bsel & USART_BSEL_gm;
+}

fw/drivers/usart.h
@@ -12,11 +12,11 @@
 
 // computed with a graph based on the formulas in the datasheet and a 16MHz f_per 
 
-#define USART_BSCALE_20000BPS 0b0011 // 2's 3
-#define USART_BSEL_20000BPS 100
+#define USART_BSCALE_SPI_20000BPS 0
+#define USART_BSEL_SPI_20000BPS 799
 
-#define USART_BSCALE_100000BPS 0b0010 // 2's 2 
-#define USART_BSEL_100000BPS 4
+#define USART_BSCALE_SPI_100000BPS 0b0000 // 2's 2 
+#define USART_BSEL_SPI_100000BPS 159
 
 #define USART_BSCALE_115200BPS 0b1100 // 2's -4
 #define USART_BSEL_115200BPS 122 
@@ -37,5 +37,6 @@
 #define USART_BSEL_2000000BPS 0
 
 void usart_spi_txrx(USART_t* usart, const void* txd, void* rxd, uint16_t len);
+void usart_set_baud(USART_t* usart, uint16_t bsel, uint8_t bscale);
 
 #endif

fw/drivers/pot.c
@@ -22,8 +22,7 @@ static void pot_config_spi()
 	PORTC.DIR &= ~USART1_RXD_PIN; // set the RX pin to input
 
 	// set to a low baud rate that works with the pull-up on MISO
-	USARTC1.BAUDCTRLB = USART_BSCALE_20000BPS << USART_BSCALE_gp;
-	USARTC1.BAUDCTRLA = USART_BSEL_20000BPS & USART_BSEL_gm;
+	usart_set_baud(&USARTC1, USART_BSEL_SPI_20000BPS, USART_BSCALE_SPI_20000BPS);
 
 	// set transfer parameters
 	USARTC1.CTRLC =

fw/drivers/sd.c
@@ -123,8 +123,7 @@ int sd_init() //{{{
 	gpio_on(&PORTE, SPI_SS_PIN_bm);
 
 	// set to a low baud rate for initial SD CARD 
-	USARTE1.BAUDCTRLB = USART_BSCALE_100000BPS << USART_BSCALE_gp;
-	USARTE1.BAUDCTRLA = USART_BSEL_100000BPS & USART_BSEL_gm;
+	usart_set_baud(&USARTE1, USART_BSEL_SPI_100000BPS, USART_BSCALE_SPI_100000BPS);
 
 	// set transfer parameters
 	USARTE1.CTRLC = USART_CMODE_MSPI_gc;
@@ -178,8 +177,7 @@ int sd_init() //{{{
 	}
 
 	// set the SPI clock to a much higher rate
-	USARTE1.BAUDCTRLB = 0;
-	USARTE1.BAUDCTRLA = 1; // must divide by at least 4 so rx DMA channel can keep up
+	usart_set_baud(&USARTE1, 1, 0);
 
 	// check the OCR register to see if it's a high capacity card (V2)
 	gpio_off(&PORTE, SPI_SS_PIN_bm);

fw/drivers/sram.c
@@ -18,8 +18,7 @@ void sram_config_spi()
 	PORTC.DIR &= ~USART1_RXD_PIN; // set the RX pin to input
 
 	// set to very fast baud rate
-	USARTC1.BAUDCTRLB = 0;
-	USARTC1.BAUDCTRLA = 1; // must divide by at least 4 so rx DMA channel can keep up
+	usart_set_baud(&USARTC1, 1, 0);
 
 	PORTC.PIN5CTRL |= PORT_INVEN_bm; // invert the SCK pin for SPI mode 3
 

fw/drivers/uart.c
@@ -100,8 +100,7 @@ void uart_init() //{{{
 	PORTD.DIR &= ~USART0_RXD_PIN; // set the RX pin to input
 
 	// set baud rate with BSEL and BSCALE values
-	USARTD0.BAUDCTRLB = USART_BSCALE_2000000BPS << USART_BSCALE_gp;
-	USARTD0.BAUDCTRLA = USART_BSEL_2000000BPS & USART_BSEL_gm;
+	usart_set_baud(&USARTD0, USART_BSEL_2000000BPS, USART_BSCALE_2000000BPS);
 
 	USARTD0.CTRLC = USART_CMODE_ASYNCHRONOUS_gc | USART_PMODE_DISABLED_gc | USART_CHSIZE_8BIT_gc; // set transfer parameters
 	USARTD0.CTRLA = USART_RXCINTLVL_HI_gc; // interrupt for receive 

commit 5142776924237c3fab38bcd4851a13032cefca65

Fixed #49 - Regression where INIT during reset causes UART to fail.
This regression came in 42fbb5ac, where I introduced a function for
setting UART baud rate. It turns out that in the UART init, setting
the baud rate in a function causes the UART to fail after reset. Instead
the baud rate setting function is now included as a static inline function.

fw/drivers/usart.c
@@ -30,9 +30,3 @@ void usart_spi_txrx(USART_t* usart, const void* txd, void* rxd, uint16_t len)
 			usart->DATA;
 	}
 }
-
-void usart_set_baud(USART_t* usart, uint16_t bsel, uint8_t bscale)
-{
-	usart->BAUDCTRLB = (bscale << USART_BSCALE_gp) | (bsel >> 8);
-	usart->BAUDCTRLA = bsel & USART_BSEL_gm;
-}

fw/drivers/usart.h
@@ -37,6 +37,12 @@
 #define USART_BSEL_2000000BPS 0
 
 void usart_spi_txrx(USART_t* usart, const void* txd, void* rxd, uint16_t len);
-void usart_set_baud(USART_t* usart, uint16_t bsel, uint8_t bscale);
+
+// this must be inlined or the UART will not init after a reboot properly
+static inline void usart_set_baud(USART_t* usart, uint16_t bsel, uint8_t bscale)
+{
+	usart->BAUDCTRLB = ((bscale << USART_BSCALE_gp) & 0xF0) | ((bsel >> 8) & 0x0F);
+	usart->BAUDCTRLA = bsel & USART_BSEL_gm;
+}
 
 #endif